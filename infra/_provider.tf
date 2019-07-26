83fa6d96e498ff9c17c02692b4c96e717440d8bf
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-26 20:30:12 +0200

    Enable OSS state storage

--- infra/_provider.tf
+++ infra/_provider.tf
@@ -11,3 +11,14 @@ provider "alicloud" {
   region = var.region
   version = "~>1.52"
 }
+
+terraform {
+  backend "oss" {
+    bucket = "ali-serverless-backend-state"
+    prefix = "state"
+    key    = "terraform.tfstate"
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+  }
+}
