cffe265260df03542ebb69338e9e48c918a2f710
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-30 21:21:34 +0200

    Update FC code

--- src/profile/function.py
+++ src/profile/function.py
@@ -1,7 +1,11 @@
 import json
+from typing import Dict, Any
+
+Event = Dict[str, Any]
+Context = Dict[str, Any]
 
 
-def handler(event, context):
+def handler(event: Event) -> str:
     print("Got event:", event)
 
     response = {
