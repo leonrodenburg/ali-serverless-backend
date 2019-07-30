af2d9a831625db3024cfa5aa0ca5c302187c75f9
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-30 21:28:51 +0200

    Split off GET request in FC

--- src/profile/function.py
+++ src/profile/function.py
@@ -5,15 +5,34 @@ Event = Dict[str, Any]
 Context = Dict[str, Any]
 
 
-def handler(event: Event) -> str:
-    print("Got event:", event)
-
-    response = {
-        "isBase64Encoded": False,
+def get(event: Event) -> Dict[str, Any]:
+    return {
         "statusCode": 200,
         "body": {
             "success": True
         }
+    }
+
+
+def handler(event: Event, context: Context) -> str:
+    print("Got event:", event)
+
+    result: Dict[str, Any]
+    method = event["httpMethod"].upper()
+
+    if method == 'GET':
+        result = get(event)
+    else:
+        result = {
+            "statusCode": 405,
+            "body": {
+                "error": "Method Not Allowed"
+            }
+        }
+
+    response = {
+        **result,
+        "isBase64Encoded": False,
     }
 
     return json.dumps(response)
